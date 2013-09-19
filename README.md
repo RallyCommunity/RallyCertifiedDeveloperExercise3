7b36c438e487a8ee6bbe3711d39ffd6d4911c38c
Charles Ferentchak <ferentchak@gmail.com>
2013-09-19 10:28:44 +0100

    solution

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-Exercise 2 - Editable Grids
+Exercise 3 - Editable Grids
 =========================
 
 ## Overview
@@ -7,5 +7,5 @@ Exercise 2 - Editable Grids
 1. Customize your board to display
     1. Make sure that the grid shows Blocked,Requirement, and Name fields
     1. Add a filter to show stories with names that contain the letter a
-    1. Add a link column renderer that points to that defects Requirement. [hint](https://help.rallydev.com/apps/2.0rc1/doc/#!/api/Rally.nav.Manager-method-getDetailUrl)
+    1. Add a link column renderer that points to that defects Requirement. [hint](https://help.rallydev.com/apps/2.0rc1/doc/#!/api/Ext.XTemplate)
 1. Show an Ext alert box when someone clicks on the name of your defect.
--- src/App.js
+++ src/App.js
@@ -1,10 +1,41 @@
 Ext.define('CustomApp', {
     extend: 'Rally.app.App',
     componentCls: 'app',
-    items:{
-        html:''
-    },
 
     launch: function() {
+        Rally.data.ModelFactory.getModel({
+            type: 'Defect',
+            success: function(model) {
+                this.grid = this.add({
+                    xtype: 'rallygrid',
+                    model: model,
+                    columnCfgs: [
+                        "Blocked",
+                        "Requirement",
+                        "Name",
+                        "ObjectID",
+                        {text: 'Link', xtype:'templatecolumn',
+                            tpl:new Ext.XTemplate(
+                            '<a href="https://rally1.rallydev.com/#/5924772439d/detail/userstory/{Requirement.ObjectID}">{Requirement._refObjectName}</a>'
+                        )}
+                    ],
+                    listeners:{
+                     itemclick:function(){
+                         alert("","Test");
+                     }
+                    },
+                    storeConfig: {
+                        filters: [
+                            {
+                                property: 'Name',
+                                operator: 'contains',
+                                value: 'a'
+                            }
+                        ]
+                    }
+                });
+            },
+            scope: this
+        });
     }
 });
